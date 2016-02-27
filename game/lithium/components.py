0460ceaaf4f2bd1398d72e9ee2b3e4530ac90dbc
Mitchell Stokes <mogurijin@gmail.com>
2016-02-27 12:50:23 -0800

    Start setting up the ECS

---
+++ game/lithium/components.py
@@ -0,0 +1,102 @@
+from panda3d import core as p3d
+from panda3d import bullet
+from bamboo import ecs
+
+
+class TemplateFactory:
+    def __init__(self, ecsmanager):
+        self.ecsmanager = ecsmanager
+
+    def make_character(self, modelpath, initial_position=None):
+        spacenp = self.ecsmanager.space.get_component('NODEPATH').nodepath
+        entity = self.ecsmanager.create_entity()
+
+        np_component = NodePathComponent(modelpath, spacenp, '**/Character')
+        if initial_position is not None:
+            np_component.nodepath.set_pos(initial_position)
+        entity.add_component(np_component)
+
+        char_component = CharacterComponent()
+        entity.add_component(char_component)
+
+        return entity
+
+class NodePathComponent(ecs.Component):
+    __slots__ = [
+        'nodepath',
+        '_modelpath'
+    ]
+
+    typeid = 'NODEPATH'
+
+    def __init__(self, modelpath=None, parent=None, filter=None):
+        super().__init__()
+        self._modelpath = modelpath if modelpath else ''
+        if modelpath is not None:
+            np = base.loader.loadModel(modelpath)
+            if filter is not None:
+                np = np.find(filter)
+                #TODO error handling
+            self.nodepath = np
+        else:
+            self.nodepath = p3d.NodePath(p3d.PandaNode('node'))
+
+        if parent is not None:
+            self.nodepath.reparent_to(parent)
+
+    def __del__(self):
+        super().__del__()
+        self.nodepath.remove_node()
+
+
+class CharacterComponent(ecs.Component):
+    __slots__ = [
+        'movement',
+    ]
+
+    typeid = 'CHARACTER'
+
+    def __init__(self):
+        super().__init__()
+        self.movement = p3d.LVector3(0, 0, 0)
+
+
+class CharacterSystem(ecs.System):
+    component_types = [
+        'CHARACTER',
+    ]
+
+    def update(self, dt, components):
+        for char in components['CHARACTER']:
+            np = char.entity.get_component('NODEPATH').nodepath
+
+            np.set_pos(np.get_pos() + char.movement)
+
+
+class PhysicsSystem(ecs.System):
+    __slots__ = [
+        'physics_world',
+        'enable_debug',
+        '_debugnp',
+    ]
+
+    component_types = [
+    ]
+
+    def __init__(self):
+        super().__init__()
+
+        self.physics_world = bullet.BulletWorld()
+        phydebug = bullet.BulletDebugNode('Physics Debug')
+        phydebug.show_wireframe(True)
+        phydebug.show_bounding_boxes(True)
+        self._debugnp = p3d.NodePath(phydebug)
+        self._debugnp.show()
+        self.physics_world.set_debug_node(phydebug)
+
+    def set_debug(np, enable):
+        if enable and not np.is_ancestor_of(self._debugnp):
+            self._debugnp.reparent_to(np)
+        elif not enable and np.is_ancestor_of(self._debugnp):
+            self._debugnp.detach_node()
+
--- game/main.py
+++ game/main.py
@@ -5,8 +5,11 @@ import sys
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 from direct.showbase.ShowBase import ShowBase
+from direct.showbase.DirectObject import DirectObject
 import panda3d.core as p3d
 import blenderpanda
+from bamboo.ecs import ECSManager, Entity
+from lithium import components
 
 
 # Load config files
@@ -18,18 +21,70 @@ else:
     print("Did not find a user config")
 
 
+
+class GameState(DirectObject):
+    def __init__(self):
+        # Setup a space to work with
+        base.ecsmanager.space = Entity(None)
+        base.ecsmanager.space.add_component(components.NodePathComponent())
+        spacenp = base.ecsmanager.space.get_component('NODEPATH').nodepath
+        spacenp.reparent_to(base.render)
+
+        # Load assets
+        self.level = loader.load_model('cathedral.bam')
+        level_start = self.level.find('**/PlayerStart')
+        self.level.reparent_to(base.render)
+        for light in self.level.find_all_matches('**/+Light'):
+            base.render.set_light(light)
+
+        self.player = base.template_factory.make_character('character.bam', level_start.get_pos())
+
+        # Attach assets to the scene graph
+        self.level.reparent_to(base.render)
+
+        def update_movement(activate):
+            char = self.player.get_component('CHARACTER')
+            if activate:
+                char.movement = p3d.LVector3(1, 0, 0)
+            else:
+                char.movement = p3d.LVector3(0, 0, 0)
+
+        self.accept('space', update_movement, [True])
+        self.accept('space-up', update_movement, [False])
+
+    def update(self, dt):
+        pass
+
+
 class GameApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         blenderpanda.init(self)
         self.accept('escape', sys.exit)
-        self.disableMouse()
 
-        self.level = self.loader.load_model('cathedral.bam')
-        self.level.reparent_to(self.render)
+        # Setup ECS
+        self.ecsmanager = ECSManager()
+        systems = [
+            components.CharacterSystem(),
+        ]
 
-        level_camera = self.level.find('**/Camera')
-        self.camera.set_pos(level_camera.get_pos())
+        for system in systems:
+            self.ecsmanager.add_system(system)
+
+        self.template_factory = components.TemplateFactory(self.ecsmanager)
+
+        def run_ecs(task):
+            self.ecsmanager.update(globalClock.get_dt())
+            return task.cont
+        self.taskMgr.add(run_ecs, 'ECS')
+
+        # Setup initial game state
+        self.game_state = GameState()
+        def run_gamestate(task):
+            self.game_state.update(globalClock.get_dt())
+            return task.cont
+        self.taskMgr.add(run_gamestate, 'GameState')
+
 
 
 app = GameApp()
